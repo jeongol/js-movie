3fd6678dfc3ea77f43b5f344cc57cd250a100795
jeongol <jungcode127@gmail.com>
2024-09-29 23:28:22 +0900

    과제 조건 최종 달성

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
-
+import { generateMovieCards } from "./movie.js";
+import { handleSearch } from "./search.js";
 
 generateMovieCards();
 
+const searchInput = document.querySelector("#search-input");
+
+searchInput.focus();
+
+// 메뉴 페이지로 이동
+function returnMenu() {
+  window.location.href = '/menu.html'; 
+}
+
+const form = document.querySelector("#search-form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  handleSearch(searchInput.value);
+});
